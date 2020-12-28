5f6e03659904e6acfedff33e79ce80db7a9b5500
Wayne Jones <waynejones@nyu.edu>
2020-12-27 19:16:43 -0500

    Add Gatsby Plugins for purgecss and remote images

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,17 @@ module.exports = {
     plugins: [
 
         `gatsby-plugin-postcss`,
+        { 
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+                printRejected: true, // Print removed selectors and processed file names
+                develop: false, // Enable while using `gatsby develop`
+                tailwind: true, // Enable tailwindcss support
+                // whitelist: ['whitelist'], // Don't remove this selector
+                // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+                // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+            },
+        },
         /**
          *  Content Plugins
          */
@@ -189,5 +200,12 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
         `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-plugin-remote-images`,
+            options: {
+                nodeType: `GhostPage`,
+                imagePath: `feature_image`,
+            },
+        },
     ],
 }
